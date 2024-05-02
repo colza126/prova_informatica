d188e547c8fd5b5e2c71c597d58ebaa7ea8598d5
Lorenzo Colzani <lollocolz04@gmail.com>
2024-05-02 16:01:08 +0200

    aggiornato database e todo

--- bicicletta.sql
+++ bicicletta.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 30, 2024 at 10:30 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Creato il: Mag 02, 2024 alle 16:00
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,21 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bicicletta`
+-- Struttura della tabella `bicicletta`
 --
 
 CREATE TABLE `bicicletta` (
   `ID` int(11) NOT NULL,
-  `posizione` varchar(255) NOT NULL,
   `stato` enum('Noleggiata','Stazione') NOT NULL,
   `codice` int(11) NOT NULL,
   `ID_stazione` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `bicicletta`
+--
+
+INSERT INTO `bicicletta` (`ID`, `stato`, `codice`, `ID_stazione`) VALUES
+(1, 'Noleggiata', 11111, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `indirizzo`
+-- Struttura della tabella `indirizzo`
 --
 
 CREATE TABLE `indirizzo` (
@@ -49,10 +55,17 @@ CREATE TABLE `indirizzo` (
   `provincia` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `indirizzo`
+--
+
+INSERT INTO `indirizzo` (`ID`, `via`, `citta`, `cap`, `provincia`) VALUES
+(1, 'via pio X', 'Mariano Comense', 22066, 'Como');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `operazione`
+-- Struttura della tabella `operazione`
 --
 
 CREATE TABLE `operazione` (
@@ -60,44 +73,49 @@ CREATE TABLE `operazione` (
   `tipo` enum('prelievo','restituzione') NOT NULL,
   `tempo` time NOT NULL,
   `ID_bicicletta` int(11) NOT NULL,
-  `ID_utente` int(11) NOT NULL,
-  `ID_stazione` int(11) NOT NULL
+  `ID_utente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stazione`
+-- Struttura della tabella `stazione`
 --
 
 CREATE TABLE `stazione` (
   `ID` int(11) NOT NULL,
-  `posizione` varchar(255) NOT NULL,
   `MaxSlot` int(11) NOT NULL,
   `ID_indirizzo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `stazione`
+--
+
+INSERT INTO `stazione` (`ID`, `MaxSlot`, `ID_indirizzo`) VALUES
+(1, 100, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utente`
+-- Struttura della tabella `utente`
 --
 
 CREATE TABLE `utente` (
   `ID` int(11) NOT NULL,
   `codice_fiscale` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `credito` int(11) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `ID_indirizzo` int(11) NOT NULL
+  `ID_indirizzo` int(11) NOT NULL,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Indici per le tabelle scaricate
 --
 
 --
--- Indexes for table `bicicletta`
+-- Indici per le tabelle `bicicletta`
 --
 ALTER TABLE `bicicletta`
   ADD PRIMARY KEY (`ID`),
@@ -105,30 +123,28 @@ ALTER TABLE `bicicletta`
   ADD KEY `ID_stazione` (`ID_stazione`);
 
 --
--- Indexes for table `indirizzo`
+-- Indici per le tabelle `indirizzo`
 --
 ALTER TABLE `indirizzo`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `operazione`
+-- Indici per le tabelle `operazione`
 --
 ALTER TABLE `operazione`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `ID_bicicletta` (`ID_bicicletta`),
-  ADD KEY `ID_utente` (`ID_utente`),
-  ADD KEY `ID_stazione` (`ID_stazione`);
+  ADD KEY `ID_utente` (`ID_utente`);
 
 --
--- Indexes for table `stazione`
+-- Indici per le tabelle `stazione`
 --
 ALTER TABLE `stazione`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `posizione` (`posizione`),
   ADD KEY `ID_indirizzo` (`ID_indirizzo`);
 
 --
--- Indexes for table `utente`
+-- Indici per le tabelle `utente`
 --
 ALTER TABLE `utente`
   ADD PRIMARY KEY (`ID`),
@@ -136,65 +152,64 @@ ALTER TABLE `utente`
   ADD KEY `ID_indirizzo` (`ID_indirizzo`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
--- AUTO_INCREMENT for table `bicicletta`
+-- AUTO_INCREMENT per la tabella `bicicletta`
 --
 ALTER TABLE `bicicletta`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `indirizzo`
+-- AUTO_INCREMENT per la tabella `indirizzo`
 --
 ALTER TABLE `indirizzo`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `operazione`
+-- AUTO_INCREMENT per la tabella `operazione`
 --
 ALTER TABLE `operazione`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `stazione`
+-- AUTO_INCREMENT per la tabella `stazione`
 --
 ALTER TABLE `stazione`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `utente`
+-- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Limiti per le tabelle scaricate
 --
 
 --
--- Constraints for table `bicicletta`
+-- Limiti per la tabella `bicicletta`
 --
 ALTER TABLE `bicicletta`
   ADD CONSTRAINT `bicicletta_ibfk_1` FOREIGN KEY (`ID_stazione`) REFERENCES `stazione` (`ID`);
 
 --
--- Constraints for table `operazione`
+-- Limiti per la tabella `operazione`
 --
 ALTER TABLE `operazione`
   ADD CONSTRAINT `operazione_ibfk_1` FOREIGN KEY (`ID_utente`) REFERENCES `utente` (`ID`),
-  ADD CONSTRAINT `operazione_ibfk_2` FOREIGN KEY (`ID_bicicletta`) REFERENCES `bicicletta` (`ID`),
-  ADD CONSTRAINT `operazione_ibfk_3` FOREIGN KEY (`ID_stazione`) REFERENCES `stazione` (`ID`);
+  ADD CONSTRAINT `operazione_ibfk_2` FOREIGN KEY (`ID_bicicletta`) REFERENCES `bicicletta` (`ID`);
 
 --
--- Constraints for table `stazione`
+-- Limiti per la tabella `stazione`
 --
 ALTER TABLE `stazione`
   ADD CONSTRAINT `stazione_ibfk_1` FOREIGN KEY (`ID_indirizzo`) REFERENCES `indirizzo` (`ID`);
 
 --
--- Constraints for table `utente`
+-- Limiti per la tabella `utente`
 --
 ALTER TABLE `utente`
   ADD CONSTRAINT `utente_ibfk_1` FOREIGN KEY (`ID_indirizzo`) REFERENCES `indirizzo` (`ID`);
