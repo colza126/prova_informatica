960efc1d0b911ae3ddbecdd49e21566e3a0c84f7
colza126 <lollocolz04@gmail.com>
2024-05-07 10:00:27 +0200

    Aggiornato gestoreBici.html e gestoreBici.js per aggiungere la funzionalità di inserimento delle biciclette.

--- script/gestoreBici.js
+++ script/gestoreBici.js
@@ -1,4 +1,21 @@
-function controllaSess(){
+function popolaSelect() {
+    var select = $("#stazioni"); // Replace "selectId" with the actual ID of your select element
+    $.ajax({
+        url: "../ajax/eliminaBici.php",
+        type: "POST",
+        success: function (response) {
+            // Assuming the response is an array of options
+            for (let index = 0; index < response.numero; index++) {
+                const element = response[index];
+                var optionElement = $("<option>").val(element.citta).text(element.citta);
+                select.append(optionElement);
+            }
+
+        }
+    });
+}
+
+function controllaSess() {
     return new Promise((resolve) => {
         $.ajax({
             url: "../ajax/controllaSessione.php",
@@ -6,10 +23,10 @@ function controllaSess(){
             success: function (data) {
                 if (data.auth) {
 
-                    if(!data.admin){
+                    if (!data.admin) {
                         window.location.href = "../index.html";
                     }
-                }else{
+                } else {
                     window.location.href = "../index.html";
                 }
             }
@@ -18,43 +35,44 @@ function controllaSess(){
 
 }
 
-function caricaBici(){
+function caricaBici() {
     var container = $("#bici");
     $.ajax({
         url: "../ajax/fetchBici.php",
         type: "POST",
-        success: function(response){
-            
+        success: function (response) {
+
             for (let index = 0; index < response.numero; index++) {
                 const element = array[index];
-                var singolo = "<div id = "+array[index].ID+">Codice:"+array[index].codice+"<br>stato:"+array[index].stato+"<br>Citta:"+array[index].citta+"<button class = 'elimina'>elimina</button>"+"</div>"
+                var singolo = "<div id = " + array[index].ID + ">Codice:" + array[index].codice + "<br>stato:" + array[index].stato + "<br>Citta:" + array[index].citta + "<button class = 'elimina'>elimina</button>" + "</div>"
                 container.append(singolo);
             }
         }
     });
 }
 
-function eliminaBici(id){
+function eliminaBici(id) {
     $.ajax({
         url: "../ajax/eliminaBici.php",
         type: "POST",
         data: { id_bici: id },
-        success: function(response){
-            if(response.status == "success"){
+        success: function (response) {
+            if (response.status == "success") {
                 alert("Bici eliminata");
                 location.reload();
-            }else{
+            } else {
                 alert("Errore");
             }
         }
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     controllaSess();
+    popolaSelect();
     caricaBici();
 
-    $(".elimina").on("click", function(){
+    $(".elimina").on("click", function () {
         var id = $(this).parent().attr("id");
         eliminaBici(id);
     });
